962709ddeaa5e884be3a3cc72b5197383b5c4551
David Drobner <david@drobner.ca>
2023-05-05 17:50:18 -0400

    Working sensor mean plot, no formatting yet

--- plotter.py
+++ plotter.py
@@ -1,1 +1,13 @@
-#temp
+from temperaturestring import TemperatureString
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+class Plotting:
+    def __init__(self, date_from, date_to):
+        self.tempstring = TemperatureString(date_from, date_to)
+
+    def averagePlot(self, indices: list) -> None:
+        fig, ax = plt.subplots(figsize=(20, 10))
+        ax.plot(self.tempstring.getTimes().apply(datetime.fromtimestamp), self.tempstring.indicesMean(indices), color="black")
+        fig.savefig("plots/testplot.png")
+
