2b181019842ad6fc6db7ccb6c25373ade585800f
David Drobner <david@drobner.ca>
2023-05-08 16:25:44 -0400

    Work on filling blank timestamps, currently broken still

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,19 +1,11 @@
 import pandas as pd
 from copy import copy
+from numpy import nan
 
 def fill_blank_timestamps(dataframe: pd.DataFrame) -> pd.DataFrame:
-    # returning the original dataframe now until I get back to this
-    return dataframe
-    # Not sure if this is right, leaving this for later
-    # want to try and output some plots first and see what I get
-    """
+    # copying as to not modify the dataframe stored in the sensor object from this function, instead taking it as a return
     df = copy(dataframe)
-    for i in range(len(df)):
-        timestamp_delta = df["Timestamp"][i+1] - df["Timestamp"][i]
-        if timestamp_delta > 1:
-            for x in range(timestamp_delta):
-                # NOTE: assumes sensor index is the same for the whole dataframe
-                # it should really be though, just based on how the whole program is structured
-                df = df.append({"Timestamp":df["Timestamp"][i]+x, "Sensor Index":df["Sensor Index"][0], "Temperature": 0}, ignore_index=True)
+    dr = pd.date_range(start=df["Timestamp"].min(), end=df["Timestamp"].max(), freq="15Min")
+    df.set_index('Timestamp').reindex(dr).fillna(0.0).rename_axis("Timestamp").reset_index()
+    print(df)
     return df
-    """
