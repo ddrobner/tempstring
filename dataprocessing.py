52e29b4d273043d7cdbe737dd701bef008d719de
David Drobner <david@drobner.ca>
2023-05-09 10:35:11 -0400

    Tweak filling frequency

--- dataprocessing.py
+++ dataprocessing.py
@@ -13,7 +13,7 @@ def fill_blank_timestamps(dataframe: pd.DataFrame) -> pd.DataFrame:
     for t in range(len(df["Timestamp"])-1):
         # so great that pandas lets you just subtract dates, love how lazy python lets you be
         if df["Timestamp"][t+1] - df["Timestamp"][t] > pd.Timedelta(1, "day"):
-            missing_dates = pd.date_range(df["Timestamp"][t], df["Timestamp"][t+1], freq="D")
+            missing_dates = pd.date_range(df["Timestamp"][t], df["Timestamp"][t+1], freq="10T")
             zeros_df = pd.DataFrame({"Timestamp":missing_dates, "Sensor Index":df["Sensor Index"][:len(missing_dates)], "Temperature":zeros(len(missing_dates))})
             df = pd.concat([df, zeros_df]).sort_values(by="Timestamp")
     return df
