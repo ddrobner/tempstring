0d8777c3d2735cb0d27ce4625ed80ec7cdf20883
David Drobner <david@drobner.ca>
2023-05-09 16:30:53 -0400

    Add argument for plot type

--- main.py
+++ main.py
@@ -13,10 +13,20 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-d', '--date-from')
 parser.add_argument('-D', '--date-to')
+parser.add_argument('--average', help="Plot the average temperature for the given indices", default=None, nargs=2)
+parser.add_argument('--index', help="Plot the temperature for a given index", default=None)
+parser.add_argument('--multiple-index', help="Plot multiple sensors in one plot", default=None, nargs=2)
 
 args = parser.parse_args()
 
 plotter = Plotting(dateparse.parse(args.date_from), dateparse.parse(args.date_to))
 
-plotter.compareIndexPlot([0, 1, 2, 3])
-plotter.indexPlot(1)
+for k, v in vars(args).items():
+    if v:
+        match v:
+            case args.index:
+                plotter.indexPlot(int(args.index))
+            case args.multiple_index:
+                plotter.compareIndexPlot(list(range(int(args.multiple_index[0]), int(args.multiple_index[1])+1)))
+            case args.average:
+                plotter.averagePlot(list(range(int(args.average[0]), int(args.average[1]))))
