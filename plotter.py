e6e0535bb265df4239eaa48032d660b39734878a
David Drobner <david@drobner.ca>
2023-05-10 13:45:54 -0400

    Document plotter

--- plotter.py
+++ plotter.py
@@ -9,12 +9,19 @@ from temperaturestring import TemperatureString
 
 # TODO find out how to uniformly set plot styling separate from each method 
 class Plotting:
+    """Module which handles all of the plotting of the data
+    """
     def __init__(self, date_from: datetime.date, date_to: datetime.date):
         self.date_from = date_from
         self.date_to = date_to
         self.tempstring = TemperatureString(date_from, date_to)
 
     def averagePlot(self, indices: list) -> None:
+        """Plots the average temperature of a given list of sensor indices
+
+        Args:
+            indices (list): List of sensor indices of which to plot the average
+        """
         fig, ax = plt.subplots(figsize=(20, 10))
         ax.set_title(f"Average Temperature Measured By Sensors {indices[0]}-{indices[-1]}")
         ax.margins(x=0, y=0.01, tight=True)
@@ -28,6 +35,11 @@ class Plotting:
         fig.savefig(f"plots/indicesMeanPlot_{self.date_from.date()}_{self.date_to.date()}_index[{indices[0]}-{indices[-1]}].png", bbox_inches='tight')
 
     def indexPlot(self, index: int) -> None:
+        """Plots the temperature data for a specific sensor
+
+        Args:
+            index (int): Index of the sensor to plot
+        """
         fig, ax = plt.subplots(figsize=(20, 10))
         ax.set_title(f"Temperature For Sensor {index}")
         ax.margins(x=0, y=0, tight=True)
@@ -40,6 +52,11 @@ class Plotting:
         fig.savefig(f"plots/indexPlot_{self.date_from.date()}_{self.date_to.date()}_index[{index}].png", bbox_inches='tight')
     
     def compareIndexPlot(self, indices:list) -> None:
+        """Plots multiple sensor indices on the same plot
+
+        Args:
+            indices (list): The list of sensor indices to plot
+        """
         fig, ax = plt.subplots(figsize=(20, 10))
         ax.margins(x=0, y=0.02, tight=True)
         color = iter(cm.tab20((np.linspace(0, 1, len(indices)))))
