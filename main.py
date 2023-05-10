f4bc7a7e554eac3eb35784fe1f3ccb32fbb59064
David Drobner <david@drobner.ca>
2023-05-10 13:29:42 -0400

    Change how arguments are parsed

--- main.py
+++ main.py
@@ -22,11 +22,12 @@ args = parser.parse_args()
 plotter = Plotting(dateparse.parse(args.date_from), dateparse.parse(args.date_to))
 
 for k, v in vars(args).items():
-    if v:
-        match v:
-            case args.index:
-                plotter.indexPlot(int(args.index))
-            case args.multiple_index:
-                plotter.compareIndexPlot(list(range(int(args.multiple_index[0]), int(args.multiple_index[1])+1)))
-            case args.average:
-                plotter.averagePlot(list(range(int(args.average[0]), int(args.average[1])+1)))
+    match v:
+        case None:
+            pass
+        case args.index:
+            plotter.indexPlot(int(args.index))
+        case args.multiple_index:
+            plotter.compareIndexPlot(list(range(int(args.multiple_index[0]), int(args.multiple_index[1])+1)))
+        case args.average:
+            plotter.averagePlot(list(range(int(args.average[0]), int(args.average[1])+1)))
