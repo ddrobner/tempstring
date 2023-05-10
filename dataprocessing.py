80370feaac8b532965b520d9649ba71695dc76f2
David Drobner <david@drobner.ca>
2023-05-10 10:34:20 -0400

    Handle DST when filling missing dates

--- dataprocessing.py
+++ dataprocessing.py
@@ -8,8 +8,8 @@ def fill_blank_timestamps(dataframe: pd.DataFrame) -> pd.DataFrame:
     # going to generate a dataframe at the missing dates with all 0's for temperature data and merge the dataframes
     for t in range(len(df["Timestamp"])-1):
         # so great that pandas lets you just subtract dates, love how lazy python lets you be
-        if df["Timestamp"][t+1] - df["Timestamp"][t] > pd.Timedelta(1, "day"):
-            missing_dates = pd.date_range(df["Timestamp"][t], df["Timestamp"][t+1], freq="10T")
+        if df["Timestamp"][t+1].tz_localize(None) - df["Timestamp"][t].tz_localize(None) > pd.Timedelta(1, "day"):
+            missing_dates = pd.date_range(df["Timestamp"][t].tz_convert("America/Toronto"), df["Timestamp"][t+1].tz_convert("America/Toronto"), freq="10T")
             zeros_df = pd.DataFrame({"Timestamp":missing_dates, "Sensor Index":df["Sensor Index"][:len(missing_dates)], "Temperature":zeros(len(missing_dates))})
             df = pd.concat([df, zeros_df]).sort_values(by="Timestamp")
     return df
