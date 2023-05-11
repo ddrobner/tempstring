7093b7922c14de8c41c879fa875c7734fe52548a
David Drobner <david@drobner.ca>
2023-05-11 12:12:53 -0400

    Document init_sensor

--- temperaturestring.py
+++ temperaturestring.py
@@ -1,6 +1,5 @@
 from sensor import Sensor
 from dbhandler import DatabaseHandler
-from copy import copy
 
 import pandas as pd
 import numpy as np
@@ -8,8 +7,18 @@ import datetime
 
 from multiprocessing import Pool
 from multiprocessing import cpu_count
+from dataprocessing import fill_blank_timestamps
 
-def init_sensor(index, sensordata):
+def init_sensor(index: int, sensordata:pd.DataFrame) -> Sensor:
+    """Helper function to initialize a Sensor, for use with multiprocessing Pool
+
+    Args:
+        index (int): Sensor index 
+        sensordata (pd.DataFrame): Sensor data 
+
+    Returns:
+        Sensor: The initialized sensor object 
+    """
     return Sensor(index, sensordata)
 
 
@@ -72,10 +81,11 @@ class TemperatureString:
         Returns:
             np.ndarray: Array containing mean temperature at each time
         """
-        cur_data = copy(self.sensors[0].data["Temperature"]).to_numpy()
+        cur_data = self.sensors[0].data[self.sensors[0].data["Temperature"] != 0]["Temperature"].to_numpy(copy=True)
         for idx in indices[1:]:
-            cur_data += np.resize(self.sensors[idx].data["Temperature"].to_numpy(), len(cur_data))
-        return cur_data/len(indices)
+            cur_data += np.resize(self.sensors[idx].data[self.sensors[idx].data["Temperature"] != 0]["Temperature"].to_numpy(), len(cur_data))
+        # TODO figure out a better way to fill in the blank timestamps, have to do this again so as to not include zero-elements in averaging
+        return fill_blank_timestamps(cur_data/len(indices))
 
     def getTimes(self, index: int) -> pd.Series:
         """Gets the time data for a sensor
