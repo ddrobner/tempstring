9dc741cc9f3bd9f2052e89c95975b77ec7279688
David Drobner <david@drobner.ca>
2023-05-17 15:26:11 -0400

    Document globals

--- globals.py
+++ globals.py
@@ -1,3 +1,5 @@
 from globalmanager import GlobalManager
 
+# creating a single instance of the globalmanager in here that everything can pull from
+# created the global manager so it's easier in the future the avoid race conditions accessing global vars
 globalmanager = GlobalManager()
