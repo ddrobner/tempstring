abfc6819dc9507202fe723440980a084f4d4b20b
David Drobner <david@drobner.ca>
2023-05-17 14:48:13 -0400

    Unify plot formatting in plotter constructor

--- plotter.py
+++ plotter.py
@@ -15,17 +15,30 @@ class Plotting:
     """Module which handles all of the plotting of the data
     """
     def __init__(self):
+        # pull in our global variables
         self.globalmanager = globals.globalmanager
         self.date_from = self.globalmanager.getParam("date_from") 
         self.date_to = self.globalmanager.getParam("date_to")
-        self.font = {'family' : 'sans',
-        'weight' : 'bold',
-        'size'   : 20}
-        self.params = {'legend.fontsize': 'x-large',
-         'axes.labelsize': 'x-large',
-         'axes.titlesize':'x-large',
-         'xtick.labelsize':'x-large',
-         'ytick.labelsize':'x-large'}
+
+        # declare figure and axes in the constructor to unify formatting
+        self.fig, self.ax = plt.subplots(figsize=(12, 10))
+        # formatting options common to all plots
+        plt.rcParams['font.size'] = 18
+        self.ax.grid()
+        self.ax.margins(x=0, y=0.01, tight=True)
+        # changing formatting depending on plot range
+        fmt = pltdates.DateFormatter('%b') if (self.date_from - self.date_to) > pd.Timedelta(3, "m") else pltdates.DateFormatter("%Y-%m-%d")
+        self.ax.xaxis.set_major_formatter(fmt)
+        # and using mpl's auto date locators
+        self.ax.xaxis.set_major_locator(pltdates.MonthLocator() if (self.date_from - self.date_to) > pd.Timedelta(3, "m") else pltdates.AutoDateLocator())
+        self.ax.xaxis.set_minor_locator(pltdates.DayLocator())
+        # for external legend
+        self.ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Sensor")
+        self.ax.tick_params(axis="both", which="major", labelsize=14)
+        self.ax.set_xlabel("Date and Time", fontsize=18)
+        self.ax.set_ylabel("Temperature (\u00B0C)", fontsize=18)
+        plt.gcf().autofmt_xdate()
+
 
 
     def averagePlot(self, indices: list) -> None:
@@ -37,13 +50,8 @@ class Plotting:
         # TODO find a better way to get only required indices, currently this gets everything in the range which is kind of bad
         # probably should just give the indices list to temperaturestring....
         self.tempstring = TemperatureString(min(indices), max(indices))
-        fig, ax = plt.subplots(figsize=(12, 10))
         indices.sort()
-        ax.set_title(f"Average Temperature Measured By Sensors {f'{indices[0]}-{indices[-1]}' if not self.globalmanager.getParam('oldstring') else ', '.join(str(i) for i in indices) + ' on the Old String'}")
-        ax.margins(x=0, y=0.01, tight=True)
-        #plt.rc('font', **self.font)
-        #plt.rcParams.update(self.params)
-        plt.rcParams['font.size'] = 18
+        self.ax.set_title(f"Average Temperature Measured By Sensors {f'{indices[0]}-{indices[-1]}' if not self.globalmanager.getParam('oldstring') else ', '.join(str(i) for i in indices) + ' on the Old String'}")
         temperaturedata = self.tempstring.indicesMean(indices)
         # autoformatting here is pretty wacky
         # splitting this out because it's going to be a long one
@@ -51,17 +59,9 @@ class Plotting:
         # going to get the argmin and shift it by some amount to take a point where they're all actual data
         # that was the plan at least, will have to come back to this
         # I concede on this one, going to eventually add a plotoptions argument to set the limits
-        ax.set_ylim(bottom=12, top=(temperaturedata.max() + 0.1))
-        ax.plot(np.resize(self.tempstring.getTimes(indices[0]).to_numpy(), len(temperaturedata)), temperaturedata, color="black")
-        ax.xaxis.set_major_locator(pltdates.MonthLocator(bymonthday=3))
-        ax.xaxis.set_major_formatter(pltdates.DateFormatter("%b"))
-        #ax.xaxis.set_minor_locator(AutoMinorLocator(4))
-        ax.xaxis.set_minor_locator(pltdates.DayLocator(interval=7))
-        ax.grid()
-        plt.gcf().autofmt_xdate()
-        ax.set_xlabel("Date and Time", fontsize=18)
-        ax.set_ylabel("Temperature (\u00B0C)", fontsize=18)
-        fig.savefig(f"plots/meanPlot_{self.date_from.date()}_{self.date_to.date()}_index[{indices[0]}-{indices[-1]}]{'_oldstring' if self.globalmanager.getParam('oldstring') else ''}.png", bbox_inches='tight')
+        self.ax.set_ylim(bottom=12, top=(temperaturedata.max() + 0.1))
+        self.ax.plot(np.resize(self.tempstring.getTimes(indices[0]).to_numpy(), len(temperaturedata)), temperaturedata, color="black")
+        self.fig.savefig(f"plots/meanPlot_{self.date_from.date()}_{self.date_to.date()}_index[{indices[0]}-{indices[-1]}]{'_oldstring' if self.globalmanager.getParam('oldstring') else ''}.png", bbox_inches='tight')
 
     def indexPlot(self, index: int) -> None:
         """Plots the temperature data for a specific sensor
@@ -70,23 +70,12 @@ class Plotting:
             index (int): Index of the sensor to plot
         """
         self.tempstring = TemperatureString(index, index)
-        fig, ax = plt.subplots(figsize=(20, 10))
-        #plt.rc('font', **self.font)
-        #plt.rcParams.update(self.params)
-        plt.rcParams['font.size'] = 18
-        ax.set_title(f"Temperature For Sensor {index} {'on the Old String' if self.globalmanager.getParam('oldstring') else ''}")
-        ax.margins(x=0, y=0, tight=True)
+        self.ax.set_title(f"Temperature For Sensor {index} {'on the Old String' if self.globalmanager.getParam('oldstring') else ''}")
+        self.ax.margins(x=0, y=0, tight=True)
         plotdata = self.tempstring.getSensorDataByIndex(index)["Temperature"]
-        ax.plot(self.tempstring.getTimes(index), plotdata, color="black")
-        ax.set_ylim(bottom=plotdata[plotdata != 0].min()-0.1, top=plotdata.max()+0.1)
-        fmt = pltdates.DateFormatter('%b') if (self.date_from - self.date_to) > pd.Timedelta(3, "m") else pltdates.DateFormatter("%Y-%m-%d")
-        ax.xaxis.set_major_formatter(fmt)
-        ax.xaxis.set_major_locator(pltdates.MonthLocator() if (self.date_from - self.date_to) > pd.Timedelta(3, "m") else pltdates.AutoDateLocator())
-        ax.xaxis.set_minor_locator(pltdates.DayLocator())
-        ax.set_xlabel("Date", fontsize=18)
-        ax.set_ylabel("Temperature (\u00B0C)", fontsize=18)
-        plt.gcf().autofmt_xdate()
-        fig.savefig(f"plots/indexPlot_{self.date_from.date()}_{self.date_to.date()}_index[{index}]{'_oldstring' if self.globalmanager.getParam('oldstring') else ''}.png", bbox_inches='tight')
+        self.ax.plot(self.tempstring.getTimes(index), plotdata, color="black")
+        self.ax.set_ylim(bottom=plotdata[plotdata != 0].min()-0.1, top=plotdata.max()+0.1)
+        self.fig.savefig(f"plots/indexPlot_{self.date_from.date()}_{self.date_to.date()}_index[{index}]{'_oldstring' if self.globalmanager.getParam('oldstring') else ''}.png", bbox_inches='tight')
     
     def compareIndexPlot(self, indices:list) -> None:
         """Plots multiple sensor indices on the same plot
@@ -96,12 +85,7 @@ class Plotting:
         """
         # initializing things
         self.tempstring = TemperatureString(min(indices), max(indices))
-        fig, ax = plt.subplots(figsize=(12, 10))
-        ax.grid()
         indices.sort()
-        plt.rcParams['font.size'] = 18
-        ax.margins(x=0, y=0.02, tight=True)
-        color = iter(cm.tab20((np.linspace(0, 1, len(indices)))))
         # initializing these as infinities so the first iteration is always the current min/max
         # very much a math person thing to do lol
         absmin = np.Inf
@@ -117,25 +101,16 @@ class Plotting:
             cmax = d.max()
             absmin = cmin if (cmin < absmin) else absmin
             absmax = cmax if (cmax > absmax) else absmax
+        # use mpl colormaps to assign a different color to each line automatically 
+        color = iter(cm.tab20((np.linspace(0, 1, len(indices)))))
         for idx in range(len(shaped_data)):
             c = next(color)
-            ax.plot(self.tempstring.getTimes(indices[0]), shaped_data[idx], label=str(indices[idx]), color=c)
+            self.ax.plot(self.tempstring.getTimes(indices[0]), shaped_data[idx], label=str(indices[idx]), color=c)
+        # change title depending on if using the old or new string
         if self.globalmanager.getParam("oldstring"):
-            ax.set_title(f"Temperature Data for Sensors {', '.join(str(i) for i in indices)} on the Old String")
+            self.ax.set_title(f"Temperature Data for Sensors {', '.join(str(i) for i in indices)} on the Old String")
         else:
-            ax.set_title(f"Temperature Data for Sensors {indices[0]}-{indices[-1]}")
+            self.ax.set_title(f"Temperature Data for Sensors {indices[0]}-{indices[-1]}")
         # using the min and max from before to autoset the ylimits
-        ax.set_ylim(bottom=(absmin - 0.1), top=(absmax + 0.1))
-        # changing formatting depending on plot range
-        fmt = pltdates.DateFormatter('%b') if (self.date_from - self.date_to) > pd.Timedelta(3, "m") else pltdates.DateFormatter("%Y-%m-%d")
-        ax.xaxis.set_major_formatter(fmt)
-        # and using mpl's auto date locators
-        ax.xaxis.set_major_locator(pltdates.MonthLocator() if (self.date_from - self.date_to) > pd.Timedelta(3, "m") else pltdates.AutoDateLocator())
-        ax.xaxis.set_minor_locator(pltdates.DayLocator())
-        # for external legend
-        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), title="Sensor")
-        ax.tick_params(axis="both", which="major", labelsize=14)
-        ax.set_xlabel("Date and Time", fontsize=18)
-        ax.set_ylabel("Temperature (\u00B0C)", fontsize=18)
-        plt.gcf().autofmt_xdate()
-        fig.savefig(f"plots/multipleIndexPlot_{self.date_from.date()}_{self.date_to.date()}_indices[{indices[0]}-{indices[-1]}]{'_oldstring' if self.globalmanager.getParam('oldstring') else ''}.png", bbox_inches='tight')
+        self.ax.set_ylim(bottom=(absmin - 0.1), top=(absmax + 0.1))
+        self.fig.savefig(f"plots/multipleIndexPlot_{self.date_from.date()}_{self.date_to.date()}_indices[{indices[0]}-{indices[-1]}]{'_oldstring' if self.globalmanager.getParam('oldstring') else ''}.png", bbox_inches='tight')
