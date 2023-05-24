a0a93c6ee852a6347093169740517af88a21e78e
David Drobner <david@drobner.ca>
2023-05-24 10:41:57 -0400

    Use pandas copy in dataprocessing

--- dataprocessing.py
+++ dataprocessing.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from copy import copy
 from numpy import zeros
 
 def fill_blank_timestamps(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -13,7 +12,7 @@ def fill_blank_timestamps(dataframe: pd.DataFrame) -> pd.DataFrame:
     """
 
     # copying as to not modify the dataframe stored in the sensor object from this function, instead taking it as a return
-    df = copy(dataframe)
+    df = dataframe.copy()
     # going to generate a dataframe at the missing dates with all 0's for temperature data and merge the dataframes
     for t in range(len(df["Timestamp"])-1):
         # so great that pandas lets you just subtract dates, love how lazy python lets you be
@@ -25,7 +24,7 @@ def fill_blank_timestamps(dataframe: pd.DataFrame) -> pd.DataFrame:
 
 def offset_sensor_indices(ts_start: pd.Timestamp, ts_end: pd.Timestamp, dataframe: pd.DataFrame) -> pd.DataFrame:
     # copying for the same reason as above
-    df = copy(dataframe)
+    df = dataframe.copy()
     for t in range(len(df["Sensor Index"])):
         if df["Timestamp"][t].tz_convert("America/Toronto") > ts_start.tz_localize("America/Toronto") and df["Timestamp"][t].tz_convert("America/Toronto")<ts_end.tz_localize("America/Toronto"):
             df["Sensor Index"][t] = df["Sensor Index"][t] + 1
